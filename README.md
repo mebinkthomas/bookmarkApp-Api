83873410c6022d5602ebcf7fdac45e1b2224fa0d
Mebin K Thomas <mebinkthomas@gmail.com>
2022-05-26 18:10:01 +0200

    authentication

---
+++ README.md
@@ -0,0 +1,1 @@
+Exploring nest.js
---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+    
+    @Get('me')
+    getme(@GetUser() user: User){
+        return user;
+    }
+}
